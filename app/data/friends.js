78bfd41d5390f891de3be688211250844364d5a4
Allison <allisonkingwilliams@gmail.com>
2018-05-20 18:01:12 -0400

    enter survey questions and answers

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+//app.get("/blocks, function(request, response) {
+//var blocks = ["fixed", "movable", "rotating"];
+// response.json(blocks) --> will return a JSON for objects and arrays
+//reponse.send(string) --> will return a string
+// };
+
+//returns a JSON for objects and arrays
