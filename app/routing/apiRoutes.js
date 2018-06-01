6586351a6e4c0c488edff12126b20df1cb288127
Allison <allisonkingwilliams@gmail.com>
2018-05-31 20:05:01 -0400

    everything working, needs styling

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,6 @@ module.exports = function(app) {
       });
     
       app.post("/api/friends",function(req,res){
-        // console.log(req.body);
 
         var newFriend = req.body;
         var newFriendScore = req.body.userScores;
